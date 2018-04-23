05916e8f2c6883ef0164b45809d40ea63e24e1da
guoguanglu <2360889142@qq.com>
2018-04-23 22:08:34 +0800

    add string detect capital

---
+++ mycode/string/detect capital.py
@@ -0,0 +1,15 @@
+#according to three conditions, write codes
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        if word.isupper():
+            return True
+        elif word[0].isupper() and word[1:].islower():
+            return True
+        elif word.islower():
+            return True
+        else:
+            return False
