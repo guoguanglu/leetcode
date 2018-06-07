37e795533ba6fe3ce7f1394adeb4d15571ed7584
guoguanglu <2360889142@qq.com>
2018-06-07 18:17:54 +0800

    add array Product of Array Except Self

--- mycode/array/Product of Array Except Self.py
+++ mycode/array/Product of Array Except Self.py
@@ -1,1 +1,18 @@
-a=[1,2,3,4]
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        temp = 1
+        for i in range(len(nums)):
+            res.append(temp)
+            temp *= nums[i]
+
+        temp = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] = res[i] * temp
+            temp *= nums[i]
+        return res
+
